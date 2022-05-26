0e97fd99b098cbcbd440505b4662a6932535f0d4
Sinan Yumurtacı <snny@google.com>
2022-05-26 13:26:17 -0700

    cp script actually cp, not mv

--- bin/s/cp_src.sh
+++ bin/s/cp_src.sh
@@ -1,2 +1,2 @@
-mv ~/data/scripts/fema/nri/output/fema_nri_schema.mcf .
-mv ~/data/scripts/fema/nri/manual_schema.mcf .
+cp ~/data/scripts/fema/nri/output/fema_nri_schema.mcf .
+cp ~/data/scripts/fema/nri/manual_schema.mcf .
