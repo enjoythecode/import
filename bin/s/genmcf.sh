514d404d62845bb81b4d437b1c578c6b69debbf8
Sinan Yumurtacı <snny@google.com>
2022-05-27 14:23:42 -0700

    update s/lint.sh and s/genmcf.sh to the new tool location produced my 'mvn package'

--- bin/s/genmcf.sh
+++ bin/s/genmcf.sh
@@ -1,1 +1,1 @@
-java -jar datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar genmcf fema_nri_stat_vars.mcf manual_fema_nri_stat_vars.mcf fema_nri_counties.tmcf nri_counties_table.csv
+java -jar ~/import/tool/target/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar genmcf fema_nri_stat_vars.mcf manual_fema_nri_stat_vars.mcf fema_nri_counties.tmcf nri_counties_table.csv
