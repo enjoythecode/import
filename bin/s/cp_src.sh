c02ab01b9b3aaf254037e5690935880e911c0adf
Sinan Yumurtacı <snny@google.com>
2022-05-31 21:12:03 +0000

    dealing with github not accepting git lfs files on forks

--- bin/s/cp_src.sh
+++ bin/s/cp_src.sh
@@ -1,4 +1,5 @@
 cp ~/data/scripts/fema/nri/output/fema_nri_stat_vars.mcf .
-cp ~/data/scripts/fema/nri/manual_fema_nri_stat_vars.mcf .
+cp ~/data/scripts/fema/nri/output/manual_fema_nri.mcf .
 cp ~/data/scripts/fema/nri/output/fema_nri_counties.tmcf .
 cp ~/data/scripts/fema/nri/output/nri_counties_table.csv .
+head ~/data/scripts/fema/nri/output/nri_tracts_table.csv -n 10000 > nri_tracts_table.csv
--- bin/s/lint.sh
+++ bin/s/lint.sh
@@ -1,1 +1,1 @@
-java -jar ~/import/tool/target/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint fema_nri_stat_vars.mcf manual_fema_nri_stat_vars.mcf
+java -jar ~/import/tool/target/datacommons-import-tool-0.1-alpha.1-jar-with-dependencies.jar lint fema_nri_stat_vars.mcf manual_fema_nri.mcf
